57193dd707189596515c3e8d6d29009baf29aa28
nkamisarava <natallia_kamisarava@epam.com>
2016-09-20 20:16:40 +0300

    Svyatoslav_Voloshin_Spring_MVC fixed unused

--- SPRINGMVS/src/main/java/com.epam.jmp/controller/MainController.java
+++ SPRINGMVS/src/main/java/com.epam.jmp/controller/MainController.java
@@ -2,15 +2,10 @@ package com.epam.jmp.controller;
 
 import com.epam.jmp.dao.PupilDaoImpl;
 import com.epam.jmp.model.Pupil;
-import com.epam.jmp.service.PupilService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/pupils")
@@ -41,11 +36,9 @@ public class MainController {
 
     @RequestMapping(value = "/updatePupil/{id}", method = RequestMethod.GET)
     public ModelAndView updatePupil(@PathVariable int id) {
-        //return new ModelAndView("index", "command", pupilDaoImpl.getPupilById(id));
         ModelAndView m = new ModelAndView("index");
         m.addObject("command", pupilDaoImpl.getPupilById(id));
         m.addObject("pupils", pupilDaoImpl.getAllPupils());
-        //m.addObject("redirect:/pupils");
         return m;
     }
 
